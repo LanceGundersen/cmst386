f267f937513de1fff1e36cd94a3f387665253f6b
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-05-02 23:06:00 -0400

    Populate most of the home page

---
+++ exercise4/script.js
@@ -0,0 +1,13 @@
+function topFunction() {
+  document.body.scrollTop = 0
+}
+
+;(function() {
+  var cx = '015553691410864360488:tarjmuh0ny8'
+  var gcse = document.createElement('script')
+  gcse.type = 'text/javascript'
+  gcse.async = true
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(gcse, s)
+})()
